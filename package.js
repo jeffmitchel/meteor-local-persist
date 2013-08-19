c28ce130071f200e4a5cc50f9caaa294693767d9
Jeff Mitchell <jeffm@3doto.com>
2013-08-19 14:40:30 -0400

    Update for 0.6.5.

--- package.js
+++ package.js
@@ -6,12 +6,13 @@ Package.on_use(function (api) {
   api.use('underscore', 'client');
   api.use('amplify', 'client');
   api.add_files('local-persist.js', 'client');
+  api.export('LocalPersist', 'client');
 });
 
 Package.on_test(function (api) {
   api.use('underscore', 'client');
   api.use('amplify', 'client');
   api.use('tinytest', 'client');
-  api.add_files('local-persist.js', 'client');
+  api.use('local-persist', 'client');
   api.add_files('local-persist-test.js', 'client');
 });
